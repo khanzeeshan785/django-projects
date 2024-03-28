5aa0a24646cef55979bc32f159f2ed1650e88edb
khanzeeshan785 <118597382+khanzeeshan785@users.noreply.github.com>
2024-03-28 13:43:41 +0530

    Dynamic_query_constrcution

--- book/views.py
+++ book/views.py
@@ -30,12 +30,38 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+# @api_view(['POST'])
+# def find_flights(request):
+# 	flights = Flight.objects.filter(departureCity = request.data['departureCity'], arrivalCity = request.data['arrivalCity'], dateOfDeparture = request.data['dateOfDeparture'])
+# 	serializer = FlightSerializer(flights,many = True)
+# 	return Response(serializer.data)
+
+#To allow searching with any combination of the three attributes (departureCity, arrivalCity, dateOfDeparture), you can dynamically construct the query based on the provided attributes. Here's how you can modify your view to achieve this:
+from django.db.models import Q
 @api_view(['POST'])
 def find_flights(request):
-	flights = Flight.objects.filter(departureCity = request.data['departureCity'], arrivalCity = request.data['arrivalCity'], dateOfDeparture = request.data['dateOfDeparture'])
-	serializer = FlightSerializer(flights,many = True)
-	return Response(serializer.data)
- 
+    departure_city = request.data.get('departureCity')
+    arrival_city = request.data.get('arrivalCity')
+    date_of_departure = request.data.get('dateOfDeparture')
+
+    # Start with an empty query
+    query = Q()
+
+    # Dynamically add conditions if attributes are provided
+    if departure_city:
+        query &= Q(departureCity=departure_city)
+    if arrival_city:
+        query &= Q(arrivalCity=arrival_city)
+    if date_of_departure:
+        query &= Q(dateOfDeparture=date_of_departure)
+
+    # Filter flights based on the constructed query
+    flights = Flight.objects.filter(query)
+
+    serializer = FlightSerializer(flights, many=True)
+    return Response(serializer.data)
+
+
 @api_view(['POST'])
 def save_reservation(request):
     flight_id = request.data.get('flightId')
